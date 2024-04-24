68d75f28da4f641d51341dcb1037a434b7375a44
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-04-24 11:38:53 -0300

    Initial Commit

---
+++ appetizers/appetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  appetizersApp.swift
+//  appetizers
+//
+//  Created by Andres Rechimon on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct appetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
